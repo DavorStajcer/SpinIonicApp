5dd938f7947ed9025e2d8945ad382e199525b1de
Davor Štajcer <davorstajcertehnicka@gmail.com>
2021-08-16 20:05:48 +0200

    added mobile pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { MenuController, Platform } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { UserService } from './services/user/user.service';
 
@@ -11,25 +11,30 @@ import { UserService } from './services/user/user.service';
 })
 export class AppComponent {
 
-  isLoggedIn : boolean
+  public isLoggedIn : boolean
+  public isMobile : boolean 
   private userAuthSubscription : Subscription = null
 
   constructor(
     private menuController: MenuController,
     private userService: UserService,
     private router: Router,
+    private platform : Platform
   ) {
     this.onInitApp()
   }
 
   async onInitApp() {
+    this.userService.isMobile = this.platform.is("mobileweb") ||this.platform.is("mobile")
+    this.isMobile = this.userService.isMobile
     if(await this.userService.isUserAuthenticated()){
       this.isLoggedIn = true
-      await this.router.navigate(["/web/dashboard"])
-      
+     if(this.isMobile){
+        await this.router.navigate([`/mobile/tabs/dashboard`])
+     }
     }else
       this.observeAuthenticatedUser()
-  }
+  } 
 
 
   observeAuthenticatedUser(){
